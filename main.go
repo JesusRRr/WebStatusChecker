981ba36bb00931fa4b2572c98d009fce52b6bb91
JesusRRr <rolonjesus1996@gmail.com>
2021-02-08 13:34:44 -0600

    chore: declaring links to websites

--- main.go
+++ main.go
@@ -1,4 +1,16 @@
 package main
 
+import "fmt"
+
 func main() {
+	links := []string{
+		"http://google.com",
+		"http://facebook.com",
+		"http://stackoverflow.com"
+		"http://golang.org",
+	}
+
+	for _, link := range links {
+		fmt.Println(link)
+	}
 }
